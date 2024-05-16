5c8e01e45bab834435dcf9b85eff193598685815
badri ramtel <ramtel.badri@gmail.com>
2024-05-16 21:30:36 +0545

    fifth

--- SNScanada/settings.py
+++ SNScanada/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-_$-hcy#7#0+21m!bd4+dt9$0u*-b86=!7j79505js2gnh40i_4'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 SESSION_COOKIE_SECURE = False
 CSRF_COOKIE_SECURE = False
